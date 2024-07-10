cb6b07bd82472f0325b4d325a95d8d41645e430f
Michał Balicki <stiekerosiem@gmail.com>
2024-07-10 15:02:41 +0200

    fix

--- apps/game-api/src/modules/user-location/user-location.gateway.ts
+++ apps/game-api/src/modules/user-location/user-location.gateway.ts
@@ -87,7 +87,7 @@ export class UserLocationGateway {
     }
     this.userUpdateTimestamps.set(payload.userId, currentTime);
 
-    this.updatePlayerLocation(payload);
+    await this.updatePlayerLocation(payload);
 
     this.getNearbyUsers(payload).then((nearbyUsers) => {
       client.emit('otherPlayersPositions', nearbyUsers);
