8a9f3cc467483eaa118c1aab2f8e8e2c010a69fc
Michał Balicki <stiekerosiem@gmail.com>
2024-07-15 15:23:27 +0200

    set cookie sameSite to none

--- apps/game-api/src/modules/auth/auth.controller.ts
+++ apps/game-api/src/modules/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export class AuthController {
       httpOnly: true,
       path: '/',
       secure: false,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     delete user.password;
@@ -65,7 +65,7 @@ export class AuthController {
       httpOnly: true,
       path: '/',
       secure: false,
-      sameSite: 'strict',
+      sameSite: 'none',
       expires: new Date(0),
     });
 
