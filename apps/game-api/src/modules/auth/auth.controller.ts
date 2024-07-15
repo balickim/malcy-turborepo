4aad615b4ae6195407f5daa46c9da5545ff8df76
Michał Balicki <stiekerosiem@gmail.com>
2024-07-15 17:37:27 +0200

    test lax

--- apps/game-api/src/modules/auth/auth.controller.ts
+++ apps/game-api/src/modules/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export class AuthController {
       httpOnly: true,
       path: '/',
       secure: false,
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     delete user.password;
@@ -65,7 +65,7 @@ export class AuthController {
       httpOnly: true,
       path: '/',
       secure: false,
-      sameSite: 'strict',
+      sameSite: 'lax',
       expires: new Date(0),
     });
 
