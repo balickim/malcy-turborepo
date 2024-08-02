a3360758ea67e77d52c71b56df0a8cdd57794f8a
Michał Balicki <stiekerosiem@gmail.com>
2024-08-02 22:04:10 +0200

    Add missing controller

--- apps/game-api/src/modules/push-notifications/push-notifications.module.ts
+++ apps/game-api/src/modules/push-notifications/push-notifications.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from 'shared-nestjs';
 
+import { AppConfig } from '~/modules/config/appConfig';
 import { ConfigModule } from '~/modules/config/config.module';
+import { PushNotificationsController } from '~/modules/push-notifications/push-notifications.controller';
 import { PushNotificationsService } from '~/modules/push-notifications/push-notifications.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity]), ConfigModule],
-  providers: [PushNotificationsService],
+  controllers: [PushNotificationsController],
+  providers: [AppConfig, PushNotificationsService],
   exports: [PushNotificationsService],
 })
 export class PushNotificationsModule {}
